a17593626ca31acd816fab426e60d82d72c3166e
BernardoVeronese <bernardopveronese@gmail.com>
2018-06-28 05:52:13 -0300

    Updated security measures

--- intranetSD/requirements/views.py
+++ intranetSD/requirements/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.core.mail import send_mail
+from django.views.decorators.csrf import csrf_exempt
 from .forms import Fin_form
 
 # Create your views here.
