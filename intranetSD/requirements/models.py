b535193c4f63fb283ad4d120d9c75e5c728f158e
BernardoVeronese <bernardopveronese@gmail.com>
2018-06-28 05:21:36 -0300

    Updated settings file

--- intranetSD/requirements/models.py
+++ intranetSD/requirements/models.py
@@ -26,7 +26,7 @@ class Req_fin(models.Model):
     email = models.EmailField(max_length=50)
     departamento = models.CharField(max_length=10, choices=DEPARTAMENTOS)
     categoria = models.CharField(max_length=50, choices=CATEGORIA)
-    data = models.DateField(_("Date"), default=datetime.date.today)
+    #data = models.DateField(_("Date"), default=datetime.date.today)
     mensagem = models.CharField(max_length=400)
     # pub_date = models.DateTimeField('date published')
     # def __str__(self):
