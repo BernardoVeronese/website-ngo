7c75fee3c6d12348a57c3d27e5f2c79197f859e8
BernardoVeronese <bernardopveronese@gmail.com>
2018-06-28 04:55:56 -0300

    Updated settings file

--- intranetSD/intranetSD/settings.py
+++ intranetSD/intranetSD/settings.py
@@ -120,6 +120,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bernardopveronese@gmail.com'
+EMAIL_HOST_PASSWORD = '2301beisa'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
